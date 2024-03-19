280f21c909aac7b9dd3c897124d3fadb070b873b
Betsy Bersson <betsybersson@gmail.com>
2024-03-19 16:48:43 -0400

    just allow cran note

--- R/pluginValues.R
+++ R/pluginValues.R
@@ -72,8 +72,10 @@ pluginValues = function(Y,group,W = NA,X = NA){
     df = data.frame(cbind(c(ybar),c(s2/N),X))
     colnames(df)[1:2] = c("Y","VarCol")
     
-    resultML = sae::eblupSFH(df$Y ~ . - df$VarCol - 1, df$VarCol, as.data.frame(W),
-                             method="ML",data = df)
+    resultML = sae::eblupSFH(Y ~ . - VarCol - 1, 
+                             VarCol, 
+                             as.data.frame(W),
+                             method="ML", data = df)
     
     theta.eb = resultML$eblup
     mu.eb = resultML$fit$estcoef[1]
